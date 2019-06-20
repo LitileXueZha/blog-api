0f2285cebf846b4fdc0d698f4ebcad03b8900a6c
宁涛 <tao.n@shulidata.com>
2019-06-20 19:04:20 +0800

    sql修改

--- doc/20190610-init.sql
+++ doc/20190610-init.sql
@@ -1,3 +1,8 @@
+-- 关于逻辑删除破坏唯一性约束的思考
+-- 1. 另建一个表
+-- 2. 删除前进行查询
+-- -----------------------------------------------------------------------------
+
 -- 初始化
 -- 创建数据库、表
 
@@ -7,6 +12,8 @@ SET NAMES 'utf8mb4';
 -- 创建数据库
 -- 这一步直接在 shell 里做
 -- CREATE DATABASE blog DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+-- -----------------------------------------------------------------------------
+-- 字符串区分大小写，只有转化为二进制存储，指定 utf8mb4_bin
 
 -- 创建文章表 article
 CREATE TABLE `article` (
@@ -20,16 +27,17 @@ CREATE TABLE `article` (
     `bg` varchar(128) DEFAULT NULL COMMENT '配图',
 
     -- 统一型字段
-    `article_id` varchar(10) NOT NULL COMMENT '唯一短链型 id',
+    `article_id` varchar(10) NOT NULL COLLATE utf8mb4_bin COMMENT '唯一短链型 id',
     `create_at` datetime DEFAULT CURRENT_TIMESTAMP,
     `modify_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     `delete_at` datetime NULL,
     `_d` tinyint(1) DEFAULT 0 COMMENT '逻辑删除',
 
     PRIMARY KEY (`id`),
-    UNIQUE KEY `uq_article` (`article_id`),
+    -- 插入前查询
+    -- UNIQUE KEY `uq_article` (`title`, `status`, `_d`),
     -- 组合索引更高效
-    KEY `idx_article` (`status`, `category`, `_d`)
+    KEY `idx_article` (`article_id`, `_d`)
 )
 ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
@@ -44,16 +52,14 @@ CREATE TABLE `msg` (
     `avatar` varchar(128) DEFAULT NULL COMMENT '头像',
     `platform` enum('pc', 'mobile') NOT NULL COMMENT '留言平台',
 
-    `msg_id` varchar(10) NOT NULL COMMENT '唯一短链型 id',
+    `msg_id` varchar(10) NOT NULL COLLATE utf8mb4_bin COMMENT '唯一短链型 id',
     `create_at` datetime DEFAULT CURRENT_TIMESTAMP,
     `modify_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     `delete_at` datetime NULL,
     `_d` tinyint(1) DEFAULT 0 COMMENT '逻辑删除',
 
     PRIMARY KEY (`id`),
-    -- 同人只能留言一次
-    UNIQUE KEY `uq_msg` (`msg_id`, `name`),
-    KEY `idx_msg` (`_d`)
+    KEY `idx_msg` (`msg_id`, `_d`)
 )
 ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
@@ -68,15 +74,14 @@ CREATE TABLE `comment` (
     `type` tinyint(1) NOT NULL COMMENT '评论类型：0-文章、1-留言',
     `parent_id` varchar(10) NOT NULL COMMENT '关联的文章、留言等 id',
 
-    `comment_id` varchar(10) NOT NULL COMMENT '唯一短链型 id',
+    `comment_id` varchar(10) NOT NULL COLLATE utf8mb4_bin COMMENT '唯一短链型 id',
     `create_at` datetime DEFAULT CURRENT_TIMESTAMP,
     `modify_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     `delete_at` datetime NULL,
     `_d` tinyint(1) DEFAULT 0 COMMENT '逻辑删除',
 
     PRIMARY KEY (`id`),
-    UNIQUE KEY `uq_comment` (`comment_id`),
-    KEY `idx_comment` (`type`, `parent_id`, `_d`)
+    KEY `idx_comment` (`comment_id`, `parent_id`, `_d`)
 )
 ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
@@ -86,16 +91,21 @@ COMMENT='评论表';
 -- 创建标签表 tag
 CREATE TABLE `tag` (
     `id` int(4) NOT NULL AUTO_INCREMENT,
-    `name` varchar(16) NOT NULL COMMENT '标签名',
+    `name` varchar(16) NOT NULL COLLATE utf8mb4_bin COMMENT '标签名',
     `display_name` varchar(64) NOT NULL COMMENT '展示标签名',
     `click` int(8) DEFAULT 0 COMMENT '点击量',
+    `status` tinyint(1) DEFAULT 1 COMMENT '标签状态：1-可用、2-不可用',
 
-    `tag_id` varchar(10) NOT NULL COMMENT '唯一短链型 id',
     `create_at` datetime DEFAULT CURRENT_TIMESTAMP,
     `modify_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     `delete_at` datetime NULL,
     `_d` tinyint(1) DEFAULT 0 COMMENT '逻辑删除',
 
     PRIMARY KEY (`id`),
-    UNIQUE KEY `uq_tag` (`tag_id`, `name`)
+    UNIQUE KEY `uq_tag` (`name`, `_d`)
 )
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+-- 大小写敏感
+COLLATE=utf8mb4_general_ci
+COMMENT '标签表';
