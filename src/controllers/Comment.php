b1ea1dc5017a3d792121939f5eeda633e105d511
宁涛 <tao.n@shulidata.com>
2019-08-26 18:16:39 +0800

    添加评论CURD接口

--- src/controllers/Comment.php
+++ src/controllers/Comment.php
@@ -4,6 +4,10 @@
  * 评论的一系列逻辑，CURD
  */
 
+require_once __DIR__.'/../models/Comment.php';
+
+use TC\Model\Comment as MMC;
+
 class Comment extends BaseController
 {
     /**
@@ -33,7 +37,163 @@ class Comment extends BaseController
 
         // 筛选未删除数据
         $params['_d'] = 0;
+        $rows = MMC::get($params, ['limit' => $limit]);
+        $res = new Response(HttpCode::OK, $rows);
 
-        Log::debug($params);
+        $res->end();
+    }
+    
+    /**
+     * 创建评论
+     * 
+     * @param Array 请求信息
+     */
+    public static function create($req)
+    {
+        $data = $req['data'];
+        $rules = [
+            'parent_id' => [
+                'type' => 'string',
+                'required' => true,
+                'error' => '评论主体不能为空',
+            ],
+            'name' => [
+                'type' => 'string',
+                'required' => true,
+                'error' => '评论人姓名不能为空',
+            ],
+            'content' => [
+                'type' => 'string',
+                'required' => true,
+                'error' => '评论内容不能为空',
+            ],
+            'type' => [
+                'type' => 'enum',
+                'required' => true,
+                // 目前只有 0-文章、1-留言类型
+                'enum' => [0, 1],
+                'error' => '评论主体类型不正确',
+            ],
+        ];
+
+        $msg = Util::validate($data, $rules);
+
+        if ($msg) {
+            self::bad($msg);
+            return;
+        }
+
+        // 可添加的字段
+        $keys = ['parent_id', 'name', 'content', 'type'];
+        $data = Util::filter($data, $keys);
+
+        $record = MMC::add($data);
+        $res = new Response(HttpCode::OK, $record);
+
+        $res->end();
+    }
+
+    /**
+     * 获取单条评论
+     * 
+     * @param Array 请求信息
+     */
+    public static function read($req)
+    {
+        $id = $req['params']['id'];
+        $params = ['comment_id' => $id, '_d' => 0];
+
+        $rows = MMC::get($params)['items'];
+
+        // 无数据
+        if (count($rows) === 0) {
+            self::notFound();
+            return;
+        }
+
+        $res = new Response(HttpCode::OK, $rows[0]);
+
+        $res->end();
+    }
+
+    /**
+     * 更新单条评论
+     * 
+     * TODO: 仅管理后台可操作
+     * @param Array 请求信息
+     */
+    public static function update($req)
+    {
+        $id = $req['params']['id'];
+        $data = $req['data'];
+        $rules = [
+            'name' => [
+                'type' => 'string',
+                'error' => '评论人姓名不能为空',
+            ],
+            'content' => [
+                'type' => 'string',
+                'error' => '评论内容不能为空',
+            ],
+            'type' => [
+                'type' => 'enum',
+                // 目前只有 0-文章、1-留言类型
+                'enum' => [0, 1],
+                'error' => '评论主体类型不正确',
+            ],
+        ];
+
+        $msg = Util::validate($data, $rules);
+
+        // 规则校验失败
+        if ($msg) {
+            self::bad($msg);
+            return;
+        }
+
+        // 可供更新的字段
+        $keys = ['name', 'content', 'type'];
+        $data = Util::filter($data, $keys);
+
+        // 无数据，返回 400
+        if (empty($data)) {
+            self::bad('没有可更新的数据');
+            return;
+        }
+
+        $rows = MMC::set($id, $data);
+
+        // 不存在此条记录，返回 404
+        if (count($rows) === 0) {
+            self::notFound();
+            return;
+        }
+
+        $res = new Response(HttpCode::OK, $rows[0]);
+
+        $res->end();
+    }
+
+    /**
+     * 删除一条评论
+     * 
+     * TODO: 仅管理后台可操作
+     * @param Array 请求信息
+     */
+    public static function delete($req)
+    {
+        $id = $req['params']['id'];
+        $count = MMC::delete($id);
+
+        // 已被删除或不存在，返回 404
+        if ($count === 0) {
+            self::notFound();
+            return;
+        }
+
+        // 返回一个 NULL，代表着这个数据为空，被删掉了
+        $res = new Response(HttpCode::OK, NULL);
+
+        $res->end();
     }
 }
--- src/models/Comment.php
+++ src/models/Comment.php
@@ -24,17 +24,133 @@ class Comment
      */
     const FORMAT = 'comment_id as id, parent_id, name, content,
                     type, create_at';
+    
+    /**
+     * 添加一条评论
+     * 
+     * @param Array 评论数据
+     * @return Array 插入成功后的记录
+     */
+    public static function add($data)
+    {
+        $db = DB::init();
+        $tb = self::NAME;
+
+        // 唯一 id 生成
+        $data['comment_id'] = DB::shortId();
+
+        $columns = array_keys($data);
+        [$col, $placeholder] = DB::getPlaceholderByKeys($columns);
+
+        $statement = "INSERT INTO $tb ($col) VALUES ($placeholder)";
+
+        $sql = $db->prepare($statement);
+
+        foreach ($data as $key => $value) {
+            $sql->bindValue(":$key", $value);
+        }
+
+        $sql->execute();
+
+        $res = self::get(['comment_id' => $data['comment_id']]);
+
+        return $res['items'][0];
+    }
 
     /**
-     * 获取留言
+     * 获取评论
      * 
      * @param Array 查询条件
      * @param Array 额外参数。例如 LIMIT、ORDER BY
      * @return Array 留言数据。格式为 [ 'total', 'items' ]
      */
-    public static function get($params, $options)
+    public static function get($params, $options = [])
     {
         $db = DB::init();
         $tb = self::NAME;
+        $format = self::FORMAT;
+        // 分页
+        $limit = empty($options['limit']) ? '0, 10' : $options['limit'];
+
+        $columns = array_keys($params);
+        $placeholder = implode(' AND ', array_map(function ($key) {
+            return "$key = :$key";
+        }, $columns));
+
+        $statement = "SELECT SQL_CALC_FOUND_ROWS $format FROM $tb WHERE $placeholder LIMIT $limit";
+
+        $sql = $db->prepare($statement);
+
+        foreach ($params as $key => $value) {
+            $sql->bindValue(":$key", $value);
+        }
+
+        $sql->execute();
+
+        $res = $sql->fetchAll();
+        $sqlCount = $db->query("SELECT FOUND_ROWS()");
+        $count = $sqlCount->fetch();
+
+        return [
+            'total' => $count['FOUND_ROWS()'],
+            'items' => $res,
+        ];
+    }
+
+    /**
+     * 更新评论
+     * 
+     * @param String 评论 id
+     * @param Array 要更新的评论数据
+     * @return Array 更新后的记录。为空则表示数据不存在
+     */
+    public static function set($id, $data)
+    {
+        $db = DB::init();
+        $tb = self::NAME;
+
+        $columns = array_keys($data);
+        $placeholder = implode(',', array_map(function ($key) {
+            return "$key = :$key";
+        }, $columns));
+
+        // 筛选未逻辑删除
+        $statement = "UPDATE $tb SET $placeholder WHERE comment_id=:id AND _d=0";
+
+        $sql = $db->prepare($statement);
+
+        foreach ($data as $key => $value) {
+            $sql->bindValue(":$key", $value);
+        }
+        // 防 sql 注入
+        $sql->bindValue(':id', $id);
+
+        $sql->execute();
+
+        // 更新后查询，如果为空则表示数据不存在
+        $res = self::get(['comment_id' => $id, '_d' => 0]);
+
+        return $res['items'];
+    }
+
+    /**
+     * 删除评论
+     * 
+     * @param String 评论 id
+     * @return Number 受影响的行数。为 0 则表示数据不存在
+     */
+    public static function delete($id)
+    {
+        $db = DB::init();
+        $tb = self::NAME;
+        // 防 sql 注入，转义之
+        $id = $db->quote($id);
+
+        $statement = "UPDATE $tb SET _d=1 WHERE comment_id=$id";
+
+        // 执行获取受影响的行数
+        $count = $db->exec($statement);
+
+        return $count;
     }
 }
