105f77dce40bc4d6dacab5c2ede6d0fb726ff213
宁涛 <tao.n@shulidata.com>
2019-08-30 19:53:29 +0800

    搜索功能实现

--- doc/20190610-init.sql
+++ doc/20190610-init.sql
@@ -37,7 +37,10 @@ CREATE TABLE `article` (
     -- 插入前查询
     -- UNIQUE KEY `uq_article` (`title`, `status`, `_d`),
     -- 组合索引更高效
-    KEY `idx_article` (`article_id`, `_d`)
+    KEY `idx_article` (`article_id`, `_d`),
+    -- 全文检索，使用 FULLTEXT 索引。目前只检索文章表
+    -- 使用内置的分词器，ngram 支持中日韩
+    FULLTEXT KEY `ftx_article` (`title`, `summary`, `content`) WITH PARSER ngram
 )
 ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
@@ -109,3 +112,18 @@ ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
 COLLATE=utf8mb4_general_ci
 COMMENT '标签表';
+
+-- shortId 计数
+-- 通过 62 进制生成的 shortId 依靠自增数保证唯一性，时间戳太长，只能考虑保存此
+-- 自增数。持久化存储只有文件与数据库，使用文件形式需要考虑迁移的问题，需要额外的
+-- 操作处理，用数据库备份可以统一
+
+CREATE TABLE `count` (
+    `id` int(8) unsigned NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (`id`)
+)
+ENGINE=InnoDB
+AUTO_INCREMENT=4000
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_general_ci
+COMMENT='shortId 计数表';
--- src/controllers/Search.php
+++ src/controllers/Search.php
@@ -36,25 +36,65 @@ class Search extends BaseController
 
         $limit = self::getLimitByQuery($params);
         // 空格作为分词符。转化为正则
-        $qstr = str_replace(' ', '|', $q);
+        $qstr = str_replace(' ', '[\s\S]{0,20}|[\s\S]{0,20}', $q);
+        $qstr = "[\s\S]{0,20}".$qstr."[\s\S]{0,20}";
         // 匹配正则
-        $regex = "/($qstr)/iu";
+        $regex = "/$qstr/iu";
+        // $regex = "/(.{0,20}标题.{0,20})/iu";
         
         $rows = MMA::fulltextSearch($q, ['limit' => $limit]);
         $res = new Response(HttpCode::OK, $rows);
 
         $test = [];
         foreach ($rows['items'] as $v) {
-            $summary = $v['summary'] .'...'. $v['content'];
-            preg_match_all($regex, $summary, $m1, PREG_OFFSET_CAPTURE);
+            $summary = $v['summary'] . $v['content'];
+            preg_match_all($regex, $summary, $m1);
+            $m1str = implode('...', array_map(function ($a) {return trim($a);}, array_unique($m1[0])));
             $test[] = [
                 'title' => $v['title'],
                 'summary' => $summary,
-                'm1' => $m1,
+                'm1' => $m1[0],
+                'm1str' => $m1str,
             ];
         }
         Log::debug($test);
 
         $res->end();
+    }
+
+    /**
+     * 提取包含关键词的文本片段
+     * 
+     * 目前使用 `空格` 分词，后续考虑开源的分词工具，例如 jieba。
+     * 提取规则为包含关键词片段的前后 20 个字
+     * FIXME: 处理多字节字符乏力，mbstring 没有提供相关正则功能
+     * 
+     * @param String 关键词文本
+     * @param String 待提取的长文本
+     * @return String|NULL 文本片段
+     */
+    protected static function keytext($keyword, $longtext)
+    {
+        // 匹配前后20个字符
+        $key = '[\s\S]{0, 20}';
+        // 空格作为分词符
+        $reg = str_replace(' ', "$key|$key", $keyword);
+        $regex = "/$key$reg$key/iu";
+
+        preg_match_all($regex, $longtext, $matches);
+
+        $match = array_map(function ($value) {
+            // 去除前后空格
+            $value = trim($value);
+            // 去除不可读字符
+            $value = preg_replace('/[\n\b\t\v]/', '', $value);
+
+            return $value;
+        }, $matches[0]);
+
+        // 省略号为连接符
+        $text = implode('...', $match);
+
+        return $text;
     }
 }
